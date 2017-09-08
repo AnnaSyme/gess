ce72109ad1cb9d57221270b579fa129202dbed3c
AnnaSyme <anna.syme@gmail.com>
2017-09-08 15:05:22 +1000

    add install kmc to dependencies file

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -6,5 +6,10 @@ echo 'Python install'
 (
    pip install --user pylint
    pip install --user ./python
-  
+)
+
+echo 'KMC install'
+(
+wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
+tar zxvf KMC3.linux.tar.gz
 )
