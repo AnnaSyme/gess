e21b748c616157b5d35e828bb8578e90e2bdb215
AnnaSyme <anna.syme@gmail.com>
2017-09-08 16:05:32 +1000

    trying install dep file changes

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
 # Install Python dependencies
-echo this is the install dep script
+echo "this is the install dep script"
 
 
-echo 'Python install'
-(
-   pip install --user pylint
-   pip install --user ./python
-)
+#echo 'Python install'
+#(
+#   pip install --user pylint
+#   pip install --user ./python
+#)
 
 #need to install kmc here
 
-echo install KMC
-
-(
+echo "install KMC"
 wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
 tar zxvf KMC3.linux.tar.gz
-)
